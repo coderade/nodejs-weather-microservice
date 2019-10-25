657d4378039f83f862d14de77d9610f5d4469df9
Valdeci Gomes <9141708+coderade@users.noreply.github.com>
2019-10-25 05:24:00 -0300

    Refactor the service.js file

--- server/service.js
+++ server/service.js
@@ -1,25 +1,29 @@
+'use strict';
+
 const express = require('express');
-const service = express();
 const request = require('superagent');
 
-
+const service = express();
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 const OPEN_WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
-service.get('/service/:location', (req, res, next) => {
-    let locationParam = req.params.location;
+service.get('/service/:location', async (req, res) => {
+    try {
+        const locationParam = req.params.location;
 
-    request.get(OPEN_WEATHER_API_URL)
-        .query({q: locationParam})
-        .query({APPID: OPEN_WEATHER_API_KEY})
-        .query({units: 'metric'})
-        .end((err, response) => {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(404);
-            }
+        const response = await request.get(OPEN_WEATHER_API_URL)
+            .query({ q: locationParam })
+            .query({ APPID: OPEN_WEATHER_API_KEY })
+            .query({ units: 'metric' });
 
-            res.json({result: `${response.body.weather[0].description} at ${response.body.main.temp} degrees`});
-        });
+        const weatherDescription = response.body.weather[0].description;
+        const temperature = response.body.main.temp;
+
+        res.json({ result: `${weatherDescription} at ${temperature} degrees` });
+    } catch (err) {
+        console.error(`Error fetching weather data: ${err.message}`);
+        res.sendStatus(500);
+    }
 });
+
 module.exports = service;
